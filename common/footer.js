5826c3b18a0ed2c317a2726ee5406013d46600fe
jacky10001 <50270079+jacky10001@users.noreply.github.com>
2021-03-20 21:52:52 +0800

    Add My Page's

---
+++ common/footer.js
@@ -0,0 +1,5 @@
+document.write( '<footer class=\"mastfoot mt-auto\">\n' );
+document.write( '    <div class=\"inner\">\n' );
+document.write( '    <p>Built for <a href="https://getbootstrap.com/">Bootstrap</a> by Me.</p>\n' );
+document.write( '    </div>\n' );
+document.write( '</footer>' );
